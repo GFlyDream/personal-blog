4956f8fd70201daa98474af8c936afc5daf7cd7a
GeoFlyDream <75395028+GFlyDream@users.noreply.github.com>
2021-11-21 17:31:53 -0500

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -36,7 +36,7 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.png"
+        src="../images/self2.png"
         width={50}
         height={50}
         quality={95}
